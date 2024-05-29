5d7910519e9708d8122dce5d17afe5792660c251
Vadim <my@com>
2024-05-29 11:12:03 +0200

    fix item contact

--- src/components/ContactsList/ContactsList.tsx
+++ src/components/ContactsList/ContactsList.tsx
@@ -2,7 +2,7 @@ import styles from "./contactsList.module.scss";
 import { usersType } from "../../type/usersType";
 import Loader from "../Loader/Loader";
 import { getAll } from "../../servises/getAll";
-import {useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import Search from "./Search/Search";
 import { useState } from "react";
 import ContactItem from "./ContactItem/ContactItem";
@@ -10,44 +10,51 @@ import ContactItem from "./ContactItem/ContactItem";
 type Props = {
   deteilCard: (n: number) => void;
   emptyCard: (n: number) => void;
-}
+};
 
-const ContactsList = ({deteilCard, emptyCard}: Props) => {
+const ContactsList = ({ deteilCard, emptyCard }: Props) => {
   // const[preiwe, setPreiwe] = useState(false);
-  const[search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
-	const {data, isPending} = useQuery({
-		queryKey: ['list'],
-		queryFn: () => getAll()
-	})
+  const { data, isPending, error } = useQuery({
+    queryKey: ["list"],
+    queryFn: () => getAll(),
+  });
+
+  // if (error) return 'An error has occurred: ' + error.message
 
   return (
     <section className={styles.contactsList}>
-
-      <Search setSearch={setSearch} search={search}/>
+      <Search setSearch={setSearch} search={search} />
 
       <ul className={styles.list}>
-        {isPending ? 
+        {isPending ? (
           [...Array(10)].map((_, i) => <Loader key={i} />)
-        :
-        data
-        .filter((contact:usersType) => {
-          if(contact.name.toLowerCase().includes(search.toLowerCase())) {
-            return contact
-          }
-        })
-        .map((contact:usersType) => {
-          return (
-            <ContactItem key={contact.id} contact={contact} deteilCard={deteilCard} emptyCard={emptyCard}/>
-          );
-        })
-        }
-        
+        ) : error ? (
+          <li className={styles.error}>
+            <span>There was an error receiving goods.</span>
+            <p>Please try again later</p>
+            <div>&#128554;</div>
+          </li>
+        ) : (
+          data
+            .filter((contact: usersType) => {
+              if (contact.name.toLowerCase().includes(search.toLowerCase())) {
+                return contact;
+              }
+            })
+            .map((contact: usersType) => {
+              return (
+                <ContactItem
+                  key={contact.id}
+                  contact={contact}
+                  deteilCard={deteilCard}
+                  emptyCard={emptyCard}
+                />
+              );
+            })
+        )}
       </ul>
-
-     
-
-
     </section>
   );
 };
